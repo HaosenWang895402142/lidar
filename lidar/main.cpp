8009a0b135188abf6b9497f3638da3a104fa7ed4
HaosenWang895402142 <31549959+HaosenWang895402142@users.noreply.github.com>
2017-11-21 22:09:35 -0500

    Add files via upload

---
+++ lidar/main.cpp
@@ -0,0 +1,37 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "line");
+    sf::Vertex line[] =
+    {
+        sf::Vertex(sf::Vector2f(10, 10)),
+        sf::Vertex(sf::Vector2f(150, 150))
+    };
+
+    std::vector<sf::Int16> samples = line;
+    buffer.loadFromSamples($samples[0], samples.size(), 2, 44100);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(line, 2, sf::Lines);
+        window.display();
+
+        sf::SoundBuffer buffer;
+        sf::Sound sound;
+        sound::setBuffer(buffer);
+        sound.play();
+
+    }
+
+    return 0;
+}
